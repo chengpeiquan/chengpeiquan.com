4836f18e35bbca505c6e5fa1d7a221f5751c0282
chengpeiquan <chengpeiquan@chengpeiquan.com>
2021-02-17 22:25:37 +0800

    Adjust the blog's directory structure

--- src/components/blog/Detail.vue
+++ src/components/blog/Detail.vue
@@ -1,9 +1,9 @@
 <template>
   <main class="flex justify-between container md:mx-auto box-border px-4 md:mt-16 mt-4">
     <!-- 博客类 -->
-    <Blog v-if="isUseBlog" :frontmatter="frontmatter">
+    <Article v-if="isUseArticle" :frontmatter="frontmatter">
       <slot />
-    </Blog>
+    </Article>
     <!-- 博客类 -->
 
     <!-- 页面类 -->
@@ -18,7 +18,7 @@ import { useRoute } from 'vue-router'
 import isArticle from '/@libs/isArticle'
 
 const route = useRoute();
-const isUseBlog = isArticle(route);
+const isUseArticle = isArticle(route);
 const { frontmatter } = defineProps<{ frontmatter: any }>();
 </script>
 
