9f258f1214e6760ae154d6428352d77ea4e299f4
chengpeiquan <chengpeiquan@chengpeiquan.com>
2021-02-19 15:16:29 +0800

    Back up some code adjustments.

--- src/components/blog/Page.vue
+++ src/components/blog/Page.vue
@@ -29,6 +29,8 @@ const meta = [
   { property: 'og:title', content: `${title} - ${config.title}` },
   { name: 'description', content: desc }
 ]
+console.log(frontmatter);
+
 
 if ( keywords ) {
   meta.push({ name: 'keywords', content: keywords });
