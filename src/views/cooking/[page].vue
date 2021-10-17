846788feb6215843a2c688b72d53b9c41eca2e37
chengpeiquan <chengpeiquan@chengpeiquan.com>
2021-10-17 23:48:57 +0800

    chore: back up some code adjustments.

---
+++ src/views/cooking/[page].vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="flex justify-between container mx-auto md:mt-16 mt-4">
+    <!-- 文章列表 -->
+    <List />
+    <!-- 文章列表 -->
+  </main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
