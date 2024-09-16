ca8c910ccd83f2f8a02d9fbc4e2aacb693892ea0
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-09-17 01:10:58 +0800

    fix: nextjs warning

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import React from 'react'
+import Image from 'next/image'
+import { LayoutMain } from 'blackwork'
+
+export default function LocalePage() {
+  return (
+    <LayoutMain fullscreen>
+      <div className="relative flex flex-1 overflow-hidden w-full h-full">
+        <Image
+          src="https://cdn.chengpeiquan.com/img/2021/01/20210222114425.jpg?x-oss-process=image/interlace,1"
+          // src="https://cdn.chengpeiquan.com/img/2022/12/20221231235941.jpg?x-oss-process=image/interlace,1"
+          // src="https://cdn.chengpeiquan.com/img/2024/07/202407220013320.jpg?x-oss-process=image/interlace,1"
+          alt="山景房里的三只猫"
+          fill
+          sizes="100vw"
+          priority
+          style={{ objectFit: 'cover' }}
+          className="blur-sm"
+        />
+      </div>
+    </LayoutMain>
+  )
+}
