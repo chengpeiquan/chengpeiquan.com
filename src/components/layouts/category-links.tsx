f4223d8f48e402314efc8a3d4a96d279fc8a5c9f
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-09-19 00:26:02 +0800

    feat: add collapsible for category links

--- src/components/layouts/category-links.tsx
+++ src/components/layouts/category-links.tsx
@@ -1,26 +1,70 @@
 import React from 'react'
-import { clsx } from 'clsx'
 import { isUndefined } from '@bassist/utils'
 import { Separator } from 'blackwork'
 import { Link } from '@/navigation'
 import { type Locale } from '@/config/locale-config'
+import { isMobileDevice } from '@/config/middleware-config'
 import {
   type CategoryConfigItem,
   type CategoryGroup,
   allCategory,
 } from '@/config/content-config'
+import { cn } from '@/utils'
+import { Collapsible } from '../shared/collapsible'
 
 interface CategoryItem extends CategoryConfigItem {
   href: string
   active: boolean
 }
 
-export const CategoryLinks: React.FC<{
+const Links: React.FC<{
+  className?: string
+  links: CategoryItem[]
+  locale: Locale
+}> = ({ className, links, locale }) => {
+  const navCls = cn(
+    'flex flex-wrap items-end gap-x-6 gap-y-2 w-full min-h-7',
+    className,
+  )
+
+  return (
+    <nav className={navCls}>
+      {links.map((i) => {
+        const cls = cn(
+          'scroll-m-20 tracking-tight',
+          i.active ? 'text-xl font-semibold' : 'text-base',
+        )
+
+        return (
+          <Link key={i.href} href={i.href} className={cls}>
+            {i.label[locale]}
+          </Link>
+        )
+      })}
+    </nav>
+  )
+}
+
+interface CategoryLinksProps {
+  className?: string
   locale: Locale
   group: CategoryGroup
   categories: CategoryConfigItem[]
   category?: string // Active category
-}> = ({ locale, group, category, categories }) => {
+  collapsible?: boolean
+  collapsibleTitle?: string
+}
+
+const LinksContent: React.FC<CategoryLinksProps> = ({
+  locale,
+  group,
+  category,
+  categories,
+  collapsible = false,
+  collapsibleTitle = '',
+}) => {
+  const isMobile = isMobileDevice()
+
   const links = [allCategory, ...categories].map<CategoryItem>((i) => {
     const href = isUndefined(i.slug)
       ? `/${group}/1` // All
@@ -34,24 +78,24 @@ export const CategoryLinks: React.FC<{
     return { ...i, href, active }
   })
 
-  if (!categories.length) return null
+  if (collapsible && isMobile) {
+    return (
+      <Collapsible title={collapsibleTitle}>
+        <Links links={links} locale={locale} />
+      </Collapsible>
+    )
+  }
+
+  return <Links links={links} locale={locale} />
+}
+
+export const CategoryLinks: React.FC<CategoryLinksProps> = (props) => {
+  const cls = cn('flex flex-col gap-4 w-full mb-4', props.className)
+
+  if (!props.categories.length) return null
   return (
-    <div className="flex flex-col gap-4 w-full mb-4">
-      <nav className="flex items-end gap-6 w-full min-h-7">
-        {links.map((i) => {
-          const cls = clsx(
-            'scroll-m-20 tracking-tight',
-            i.active ? 'text-xl font-semibold' : 'text-base',
-          )
-
-          return (
-            <Link key={i.href} href={i.href} className={cls}>
-              {i.label[locale]}
-            </Link>
-          )
-        })}
-      </nav>
-
+    <div className={cls}>
+      <LinksContent {...props} />
       <Separator />
     </div>
   )
