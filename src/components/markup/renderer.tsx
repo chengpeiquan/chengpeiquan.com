e1ebc31f466381a1ad4d1e64f513eb8d7565ddfa
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-09-28 00:17:21 +0800

    feat(markup): add markdown renderer

---
+++ src/components/markup/renderer.tsx
@@ -0,0 +1,108 @@
+import React from 'react'
+import { getTranslations } from 'next-intl/server'
+import { Heading, Paragraph } from 'blackwork'
+import { isString } from '@bassist/utils'
+import { type ContentItem, type ContentMetadata } from '@/config/content-config'
+import { type LocalePageParams } from '@/config/locale-config'
+import { isMobileDevice } from '@/config/middleware-config'
+import { Link } from '@/navigation'
+import { MiniGithubIcon } from '../shared/icons'
+
+interface StarOnGitHubProps extends LocalePageParams {
+  repo: ContentMetadata['repo']
+}
+
+const StarOnGitHub = async ({ locale, repo }: StarOnGitHubProps) => {
+  const t = await getTranslations({
+    locale,
+    namespace: 'basicConfig',
+  })
+
+  const label = t('starOnGitHub.label')
+  const title = t('starOnGitHub.title')
+
+  if (!isString(repo)) return null
+  return (
+    <Link
+      title={title}
+      aria-label={title}
+      href={repo!}
+      className="flex items-center text-sm no-underline font-normal transition-colors hover:text-foreground text-gray-400 dark:text-gray-500"
+      target="_blank"
+      rel="nofollow noopener noreferrer"
+    >
+      <MiniGithubIcon />
+      <span className="ml-1">{label}</span>
+    </Link>
+  )
+}
+
+interface AuthorDataProps extends StarOnGitHubProps {
+  isMobile?: boolean
+  author: string
+  date: ContentMetadata['date']
+}
+
+const AuthorData = async ({
+  locale,
+  isMobile,
+  author,
+  date,
+  repo,
+}: AuthorDataProps) => {
+  const starVisible = !isMobile && repo?.startsWith('http')
+
+  return (
+    <div className="flex items-center justify-between w-full my-4">
+      <Paragraph className="text-sm text-gray-400 dark:text-gray-500 m-0">
+        <span aria-label={author}>{author}</span>
+
+        <span className="ml-6" aria-label={date}>
+          {date}
+        </span>
+      </Paragraph>
+
+      {starVisible && <StarOnGitHub locale={locale} repo={repo} />}
+    </div>
+  )
+}
+
+interface MarkupRendererProps
+  extends LocalePageParams,
+    Pick<ContentItem, 'metadata' | 'jsxElement'> {
+  toc?: React.ReactNode
+}
+
+export const MarkupRenderer = async ({
+  metadata,
+  toc,
+  jsxElement,
+  locale,
+}: MarkupRendererProps) => {
+  const isMobile = isMobileDevice()
+
+  const t = await getTranslations({
+    locale,
+    namespace: 'siteConfig',
+  })
+
+  return (
+    <article className="flex flex-col flex-1 overflow-hidden prose prose-neutral dark:prose-invert">
+      <Heading level={1} className="mb-0 text-2xl sm:text-3xl">
+        {metadata.title}
+      </Heading>
+
+      <AuthorData
+        locale={locale}
+        isMobile={isMobile}
+        author={t('name')}
+        date={metadata.date}
+        repo={metadata.repo}
+      />
+
+      {toc}
+
+      {jsxElement}
+    </article>
+  )
+}
