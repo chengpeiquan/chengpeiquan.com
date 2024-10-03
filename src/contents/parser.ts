58321faf1235b4ed3a2883d287b8b93abca440ca
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-10-03 13:54:24 +0800

    feat(contents): support dynamic management of parser plugin chains

--- src/contents/parser.ts
+++ src/contents/parser.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import React from 'react'
 import matter from 'gray-matter'
 import remarkParse from 'remark-parse'
 import remarkUnwrapImages from 'remark-unwrap-images'
@@ -14,9 +15,9 @@ import rehypeExternalLink from 'rehype-external-links'
 import rehypeSanitize from 'rehype-sanitize'
 import rehypeStringify from 'rehype-stringify'
 import rehypeReact, { type Options as RehypeReactOptions } from 'rehype-react'
-import { unified } from 'unified'
+import { type PluggableList, unified } from 'unified'
+import { readFile } from 'node:fs/promises'
 import { isArray, isObject } from '@bassist/utils'
-import { fs } from '@bassist/node-utils'
 import {
   type ContentItem,
   type ContentMetadata,
@@ -25,7 +26,6 @@ import {
 } from '@/config/content-config'
 import { a, img } from './components'
 import { Fragment, jsx as _jsx, jsxs as _jsxs } from 'react/jsx-runtime'
-import type React from 'react'
 
 const isValidHeading = (v: unknown): v is HeadingItem => isObject(v) && !!v.id
 
@@ -50,77 +50,111 @@ const rehypeReactOptions = {
   development: false,
 } satisfies RehypeReactOptions
 
-interface ParseMarkdownRes {
-  headings: HeadingItem[]
-  jsxElement: React.JSX.Element | null
-  html: string
+/**
+ * @param simplify - When generating RSS feeds or JSON files,
+ *  it will parse them in a simplified mode, and plugins such as
+ *  code highlighting or JSX transformation will not be enabled.
+ */
+const createProcessor = (simplify = false) => {
+  // Processing Markdown
+  const remarkPlugins: PluggableList = [
+    [remarkParse],
+    [remarkUnwrapImages],
+    [remarkGfm],
+    [remarkStringify],
+  ]
+
+  // Transforming Markdown to HTML
+  const remarkRehypePlugins: PluggableList = [[remarkRehype]]
+
+  // Processing HTML
+  const rehypePlugins: PluggableList = (() => {
+    if (simplify) return [[rehypeStringify]]
+
+    return [
+      [rehypeSlug, { prefix: '' }],
+      [rehypeAutolinkHeadings],
+      [rehypeExtractToc],
+      [rehypeExternalLink],
+      [
+        rehypeSanitize,
+        {
+          // No need `user-content-` prefix
+          clobberPrefix: '',
+        },
+      ],
+      [
+        rehypeShiki,
+        {
+          // https://shiki.style/themes
+          themes: {
+            light: 'one-light',
+            dark: 'dracula-soft',
+          },
+        },
+      ],
+      [rehypeStringify],
+    ]
+  })()
+
+  // Instead of this usage
+  // `import { toJsxRuntime } from 'hast-util-to-jsx-runtime'`
+  // `const mdastTree = processor.parse(file)`
+  // `const hastTree = await processor.run(mdastTree, file)`
+  // `const jsxElement = toJsxRuntime(hastTree, {...options})`
+  const reactPlugins: PluggableList = simplify
+    ? []
+    : [[rehypeReact, rehypeReactOptions]]
+
+  const processor = unified()
+    .use(remarkPlugins)
+    .use(remarkRehypePlugins)
+    .use(rehypePlugins)
+    .use(reactPlugins)
+
+  return processor
 }
+
+type ParseMarkdownRes = Pick<ContentItem, 'headings' | 'html' | 'jsxElement'>
 
 /**
  * Transforming Markdown to HTML,
  * and add slugs / anchors, and extract headings
  *
  * @param markdown - Extract from `gray-matter`
+ *
+ * @param simplify - See `createProcessor`
  */
-const parseMarkdown = async (markdown: string): Promise<ParseMarkdownRes> => {
+const parseMarkdown = async (
+  markdown: string,
+  simplify?: boolean,
+): Promise<ParseMarkdownRes> => {
   try {
-    const processor = unified()
-      // Processing Markdown
-      .use(remarkParse)
-      .use(remarkUnwrapImages)
-      .use(remarkGfm)
-      .use(remarkStringify)
-
-      // Transforming Markdown to HTML
-      .use(remarkRehype)
-
-      // Processing HTML
-      .use(rehypeSlug, { prefix: '' })
-      .use(rehypeAutolinkHeadings)
-      .use(rehypeExtractToc)
-      .use(rehypeExternalLink)
-      .use(rehypeSanitize, {
-        // No need `user-content-` prefix
-        clobberPrefix: '',
-      })
-      .use(rehypeShiki, {
-        // https://shiki.style/themes
-        themes: {
-          light: 'one-light',
-          dark: 'dracula-soft',
-        },
-      })
-      .use(rehypeStringify)
-
-      // Instead of this usage
-      // `import { toJsxRuntime } from 'hast-util-to-jsx-runtime'`
-      // `const mdastTree = processor.parse(file)`
-      // `const hastTree = await processor.run(mdastTree, file)`
-      // `const jsxElement = toJsxRuntime(hastTree, {...options})`
-      .use(rehypeReact, rehypeReactOptions)
+    const processor = createProcessor(simplify)
 
     // Instead of this usage
     // `import { read } from 'to-vfile'`
     // `.process(await read(filePath))`
     const file = await processor.process(markdown)
-    const jsxElement = file?.result || null
+
     const html = String(file)
+    const jsxElement = React.isValidElement(file?.result) ? file.result : null
 
     const rawHeadings = isArray(file?.data?.toc) ? file.data.toc : []
     const headings = rawHeadings.filter((i) => isValidHeading(i))
 
     return {
       headings,
-      jsxElement,
       html,
+      jsxElement,
     }
   } catch (e) {
     console.error('[Parse error] ', e)
 
     return {
       headings: [],
-      jsxElement: null,
       html: '',
+      jsxElement: null,
     }
   }
 }
@@ -162,20 +196,25 @@ const parseSlug = (filePath: string) => {
   return extension ? lastFragment.slice(0, -extension.length) : ''
 }
 
-interface ParseOptions {
+export interface ParseOptions {
   /**
    * When only using the introductory data for a list,
    * ignore the parsing of the detailed information.
    */
   ignoreDetails?: boolean
+
+  /**
+   * See `createProcessor`
+   */
+  simplify?: boolean
 }
 
 export const parse = async (
   filePath: string,
-  { ignoreDetails = false }: ParseOptions = {},
+  { ignoreDetails = false, simplify = false }: ParseOptions = {},
 ): Promise<ContentItem | null> => {
   try {
-    const raw = (await fs.readFile(filePath, 'utf-8')) || ''
+    const raw = (await readFile(filePath, 'utf-8')) || ''
     const { content: markdown = '', data = {} } = matter(raw)
 
     const { date: utcDate, ...rest } = data
@@ -192,19 +231,22 @@ export const parse = async (
       return {
         slug,
         headings: [],
-        jsxElement: null,
         html: '',
+        jsxElement: null,
         metadata,
       } satisfies ContentItem
     }
 
-    const { headings, jsxElement, html } = await parseMarkdown(markdown)
+    const { headings, html, jsxElement } = await parseMarkdown(
+      markdown,
+      simplify,
+    )
 
     return {
       slug,
       headings,
-      jsxElement,
       html,
+      jsxElement,
       metadata,
     } satisfies ContentItem
   } catch (e) {
