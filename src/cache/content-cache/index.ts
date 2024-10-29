b748ba7f0ae9a5b0dc0f94cd99adde9ed24e8219
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-10-30 00:00:43 +0800

    chore: debugging deployment issues

--- src/cache/content-cache/index.ts
+++ src/cache/content-cache/index.ts
@@ -1,27 +1,30 @@
-import { ContentFolder, type ListFolder } from '@/config/content-config'
+import {
+  //  ContentFolder,
+  type ListFolder,
+} from '@/config/content-config'
 import { type Locale } from '@/config/locale-config'
 import {
   type CacheMapKey,
   type ContentCacheItem,
   getCacheMapKey,
 } from '@/config/cache-config'
-import article_en from './article-en.json'
-import article_zh from './article-zh.json'
-import cookbook_zh from './cookbook-zh.json'
+// import article_en from './article-en.json'
+// import article_zh from './article-zh.json'
+// import cookbook_zh from './cookbook-zh.json'
 
 const contentCacheMap = new Map<CacheMapKey, ContentCacheItem[]>([
-  [
-    getCacheMapKey(ContentFolder.Article, 'zh'),
-    article_zh as ContentCacheItem[],
-  ],
-  [
-    getCacheMapKey(ContentFolder.Article, 'en'),
-    article_en as ContentCacheItem[],
-  ],
-  [
-    getCacheMapKey(ContentFolder.Cookbook, 'zh'),
-    cookbook_zh as ContentCacheItem[],
-  ],
+  // [
+  //   getCacheMapKey(ContentFolder.Article, 'zh'),
+  //   article_zh as ContentCacheItem[],
+  // ],
+  // [
+  //   getCacheMapKey(ContentFolder.Article, 'en'),
+  //   article_en as ContentCacheItem[],
+  // ],
+  // [
+  //   getCacheMapKey(ContentFolder.Cookbook, 'zh'),
+  //   cookbook_zh as ContentCacheItem[],
+  // ],
 ])
 
 export const getContentCache = (
