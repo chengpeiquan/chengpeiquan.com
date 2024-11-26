f2b9678ba08c4fcd0542a7658b2299be1964efa0
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-11-27 00:58:17 +0800

    chore: centralized management of external information

--- src/components/sidebar/friendly-links.tsx
+++ src/components/sidebar/friendly-links.tsx
@@ -1,37 +1,14 @@
 import React from 'react'
 import { Separator } from 'blackwork'
 import { Link } from '@/navigation'
-import { type linkItem } from '@/config/route-config'
 import { SidebarBlock } from '../shared/sidebar-block'
-
-const links: readonly linkItem[] = [
-  {
-    href: 'https://vue3.chengpeiquan.com',
-    title: 'Vue3 入门指南与实战案例',
-  },
-  {
-    href: 'https://www.fnnas.com',
-    title: '飞牛 NAS',
-  },
-  {
-    href: 'https://chawyehsu.com',
-    title: 'The Art of Chawye Hsu',
-  },
-  {
-    href: 'https://guangne.com',
-    title: '旅行家龙猫',
-  },
-  {
-    href: 'https://muki.tw',
-    title: 'Muki Space',
-  },
-]
+import { friendlyLinks } from '@/config/external-config'
 
 export const FriendlyLinks: React.FC = () => {
   return (
     <SidebarBlock title="友情链接">
       <div className="w-full space-x-3 space-y-2">
-        {links.map((i, idx) => {
+        {friendlyLinks.map((i, idx) => {
           return (
             <React.Fragment key={i.href}>
               {idx > 0 && (
---
+++ src/config/external-config.ts
@@ -0,0 +1,24 @@
+import { type linkItem } from './route-config'
+
+export const friendlyLinks: readonly linkItem[] = [
+  {
+    href: 'https://vue3.chengpeiquan.com',
+    title: 'Vue3 入门指南与实战案例',
+  },
+  {
+    href: 'https://www.fnnas.com',
+    title: '飞牛私有云 fnOS',
+  },
+  {
+    href: 'https://chawyehsu.com',
+    title: 'The Art of Chawye Hsu',
+  },
+  {
+    href: 'https://guangne.com',
+    title: '旅行家龙猫',
+  },
+  {
+    href: 'https://muki.tw',
+    title: 'Muki Space',
+  },
+]
