1c5109650574f64568787c6c18a1bfb8cfedb53e
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-11-29 23:06:29 +0800

    test(deploy): env vars

---
+++ src/app/[locale]/test/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function TestPage() {
+  return (
+    <div>
+      <p>NEXT_PUBLIC_HELLO: {process.env.NEXT_PUBLIC_HELLO}</p>
+      <p>NEXT_PUBLIC_WORLD: {process.env.NEXT_PUBLIC_WORLD}</p>
+      <p>NEXT_PUBLIC_HELLO_WORLD: {process.env.NEXT_PUBLIC_HELLO_WORLD}</p>
+    </div>
+  )
+}
