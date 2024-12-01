a633d682281d6dc95adc58706b1ae667d1f4b8e0
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-12-01 22:52:29 +0800

    test(deploy): env

--- src/app/[locale]/test/page.tsx
+++ src/app/[locale]/test/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 export default function TestPage() {
+  const val = !!process.env.HELLO_WORLD
+
   return (
     <div>
       <p>NEXT_PUBLIC_HELLO: {process.env.NEXT_PUBLIC_HELLO}</p>
       <p>NEXT_PUBLIC_WORLD: {process.env.NEXT_PUBLIC_WORLD}</p>
       <p>NEXT_PUBLIC_HELLO_WORLD: {process.env.NEXT_PUBLIC_HELLO_WORLD}</p>
-      <p>HELLO_WORLD: {process.env.HELLO_WORLD}</p>
+      <p>HELLO_WORLD: {val ? 'true' : 'false'}</p>
     </div>
   )
 }
--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,9 +1,11 @@
 export async function GET() {
+  const val = !!process.env.HELLO_WORLD
+
   const data = {
     NEXT_PUBLIC_HELLO: process.env.NEXT_PUBLIC_HELLO,
     NEXT_PUBLIC_WORLD: process.env.NEXT_PUBLIC_WORLD,
     NEXT_PUBLIC_HELLO_WORLD: process.env.NEXT_PUBLIC_HELLO_WORLD,
-    HELLO_WORLD: process.env.HELLO_WORLD,
+    HELLO_WORLD: val ? 'true' : 'false',
   }
 
   return Response.json({
