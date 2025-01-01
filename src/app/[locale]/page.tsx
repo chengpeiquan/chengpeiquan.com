7ddfcf06f1be978723a79163c7769ae1e770c2e1
chengpeiquan <chengpeiquan@chengpeiquan.com>
2025-01-01 18:16:13 +0800

    chore: typo

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -12,6 +12,7 @@ export default function LocalePage() {
           src="https://cdn.chengpeiquan.com/img/2021/01/20210222114425.jpg?x-oss-process=image/interlace,1"
           // src="https://cdn.chengpeiquan.com/img/2022/12/20221231235941.jpg?x-oss-process=image/interlace,1"
           // src="https://cdn.chengpeiquan.com/img/2024/07/202407220013320.jpg?x-oss-process=image/interlace,1"
+          // src="https://cdn.chengpeiquan.com/img/2025/01/202501011808403.jpg?x-oss-process=image/interlace,1"
           alt="山景房里的三只猫"
           fill
           sizes="100vw"
