3a1b9a4a4f32c4a026f95468b9a1f638d65dcf05
chengpeiquan <chengpeiquan@chengpeiquan.com>
2025-03-31 01:22:42 +0800

    feat(home): add random cookbooks display

--- src/app/[locale]/(home)/components/shared-widgets.tsx
+++ src/app/[locale]/(home)/components/shared-widgets.tsx
@@ -11,8 +11,8 @@ export const SectionTitle: React.FC<SectionTitleProps> = ({
   description,
 }) => {
   return (
-    <div className="mb-24 flex w-full flex-col gap-4">
-      <h2 className="text-foreground text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
+    <div className="mb-12 flex w-full flex-col gap-4 sm:mb-16 md:mb-24">
+      <h2 className="text-foreground text-4xl font-bold sm:text-5xl lg:text-6xl xl:text-7xl">
         {title}
       </h2>
       <p className="text-muted-foreground ml-[2.5em] text-base sm:text-xl md:text-2xl">
@@ -25,5 +25,7 @@ export const SectionTitle: React.FC<SectionTitleProps> = ({
 export const SectionContainer: React.FC<React.PropsWithChildren> = ({
   children,
 }) => {
-  return <div className="container relative py-28">{children}</div>
+  return (
+    <div className="container relative py-16 md:py-20 lg:py-28">{children}</div>
+  )
 }
