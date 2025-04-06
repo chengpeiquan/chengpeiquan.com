3b9f127087cb82eda4f0cb677ef2cbf66afcdf32
chengpeiquan <chengpeiquan@chengpeiquan.com>
2025-04-06 16:01:38 +0800

    feat(middleware): strip all search parameters from the url

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,22 @@
-import { type NextRequest, userAgent } from 'next/server'
+import { type NextRequest, NextResponse, userAgent } from 'next/server'
 import createMiddleware from 'next-intl/middleware'
 import { defaultHeaderValue, headerFields } from '@/config/middleware-config'
 import { routing } from '@/i18n/routing'
 
 export default async function middleware(request: NextRequest) {
+  // Strip all search parameters from the URL to prevent reflected XSS attacks.
+  // This helps avoid scenarios where malicious query strings
+  // are rendered directly in SSR pages, leading to potential security vulnerabilities.
+  // Since this site currently does not rely on search parameters for rendering,
+  // Now enforce a redirect to the same URL without any query string.
+  // If necessary, only whitelisted parameters should be allowed
+  // in pages that intentionally use searchParams (e.g., search pages).
+  const url = request.nextUrl.clone()
+  if ([...url.searchParams.keys()].length > 0) {
+    url.search = ''
+    return NextResponse.redirect(url)
+  }
+
   const { device } = userAgent(request)
 
   const handleI18nRouting = createMiddleware(routing)
