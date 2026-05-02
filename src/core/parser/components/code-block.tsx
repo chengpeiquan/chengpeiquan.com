8cedacc0d9a103bfc2f949bdb9784bbf2634b15c
chengpeiquan <chengpeiquan@chengpeiquan.com>
2026-05-02 23:16:10 +0800

    fix(layout): resolve mobile layout regressions

--- src/core/parser/components/code-block.tsx
+++ src/core/parser/components/code-block.tsx
@@ -6,10 +6,12 @@ import {
 } from '@blackwork/machine'
 import { useTranslations } from 'next-intl'
 import React from 'react'
+import { cn } from '@/utils'
 
 export type CodeBlockProps = MachineCodeBlockProps
 
 export const CodeBlock: React.FC<React.PropsWithChildren<CodeBlockProps>> = ({
+  className,
   copyLabel,
   copiedLabel,
   ...props
@@ -18,6 +20,21 @@ export const CodeBlock: React.FC<React.PropsWithChildren<CodeBlockProps>> = ({
 
   return (
     <MachineCodeBlock
+      className={cn(
+        'max-w-full',
+        'wrap-break-word whitespace-pre-wrap',
+        `
+          [&_.line]:block [&_.line]:wrap-break-word
+          [&_.line]:whitespace-pre-wrap
+        `,
+        '[&_code]:block [&_code]:w-full [&_code]:min-w-0',
+        '[&_code]:wrap-break-word [&_code]:whitespace-pre-wrap',
+        'sm:whitespace-pre',
+        'sm:[&_.line]:break-normal sm:[&_.line]:whitespace-pre',
+        'sm:[&_code]:w-max sm:[&_code]:min-w-full',
+        'sm:[&_code]:break-normal sm:[&_code]:whitespace-pre',
+        className,
+      )}
       copyLabel={copyLabel ?? t('copy')}
       copiedLabel={copiedLabel ?? t('copied')}
       {...props}
